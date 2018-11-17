59be15fa32fb4cd20b8b0941fe2c2a7c501176e8
brennoDuarte <brenno.gnr@gmail.com>
2018-11-17 08:43:11 -0300

    first commit

---
+++ src/br/com/sysadv/dal/Conexao.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.sysadv.dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author brenno
+ */
+public class Conexao {
+    public static Connection Conectar(){
+        java.sql.Connection conexao = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/db_sysadv";
+        String user = "root";
+        String password = "";
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
